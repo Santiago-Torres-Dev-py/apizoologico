41928d74153cf45e43b5295af133954a88efbb35
Santiago <sntglopeztorres@gmail.com>
2026-03-11 14:01:20 -0500

    implentación de express

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 'use strict';
 const http = require('http');
 
@@ -6,5 +7,15 @@ const server = http.createServer(function(req, res){
     res.end('Hola Mundo');
 });
 server.listen(5000);
+*/
+const express = require('express')
+const app = express()
+const port = 5000
 
+app.get('/', (req, res) => {
+    res.send('¡Hola Santiago!')
+})
 
+app.listen(port, () => {
+    console.log('La aplicación se esta ejecutando por el puerto', + `${port}`)
+})
